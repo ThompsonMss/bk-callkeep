8492bbb638e1f9088d8755785a888aafc67afa3b
Thompson Silva <thompson.msds@gmail.com>
2020-08-19 12:36:20 -0300

    Page Main

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,7 +1,39 @@
-const React = require('react');
+const React = require("react");
 
-function HelloMessage(props){
- return <div style={{backgroundColor: 'green'}}>OI {props.name}</div>
+const MainLayout = require("../_layouts/Main");
+
+const makeStyle = require("./styles");
+
+function Home(props) {
+  const [token, setToken] = React.useState(props.token);
+
+  const styles = makeStyle();
+
+  return (
+    <MainLayout>
+      <div style={styles.container}>
+        <div>
+          <span style={styles.title}>Call</span>
+          <span style={styles.titleNotBound}>Keep</span>
+        </div>
+
+        <div style={styles.containerForm}>
+          <form style={styles.form}>
+            <span style={styles.label}>Seu token firebase: </span>
+            <textarea
+              placeholder="Insira o seu token"
+              style={styles.input}
+              rows="5"
+              cols="40"
+              value={token}
+              onChange={(e) => setToken(e.target.value)}
+            />
+            <button style={styles.button}>Realizar chamada</button>
+          </form>
+        </div>
+      </div>
+    </MainLayout>
+  );
 }
 
-module.exports = HelloMessage;
+module.exports = Home;
---
+++ src/views/_layouts/Main/styles.js
@@ -0,0 +1,13 @@
+function makeStyle() {
+  return {
+    hb: {
+      height: "100vh",
+      margin: 0,
+      padding: 0,
+      backgroundColor: "#F6F6FA",
+      fontFamily: "Poppins, sans-serif",
+    },
+  };
+}
+
+module.exports = makeStyle;
